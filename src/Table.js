870362a1fa59b03b08b7c6f53bb2cd440b848778
Sky Lin <sky.lin@hulu.com>
2020-05-13 22:33:21 -0700

    fixed the UI

--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react';
 
 const TableHeader = () => {
     return (
@@ -6,40 +6,34 @@ const TableHeader = () => {
             <tr>
                 <th>Name</th>
                 <th>Job</th>
+                <th>Remove</th>
             </tr>
         </thead>
-    )
+    );
 }
 
-const TableBody = (props) => {
-
+const TableBody = props => {
     const rows = props.characterData.map((row, index) => {
         return (
             <tr key={index}>
                 <td>{row.name}</td>
                 <td>{row.job}</td>
-                <td>
-                    <button onClick={() => props.removeCharacter(index)}>Delete</button>
-                </td>
+                <td><button onClick={() => props.removeCharacter(index)}>Delete</button></td>
             </tr>
-        )
+        );
     });
 
     return <tbody>{rows}</tbody>;
 }
 
-class Table extends Component {
-    render() {
-
-        const { characterData, removeCharacter } = this.props;
-
-        return (
-            <table>
-                <TableHeader />
-                <TableBody characterData={characterData} removeCharacter={removeCharacter} />
-            </table>
-        )
-    }
+const Table = (props) => {
+    const { characterData, removeCharacter } = props;
+    return (
+        <table>
+            <TableHeader />
+            <TableBody characterData={characterData} removeCharacter={removeCharacter} />
+        </table>
+    );
 }
 
-export default Table
+export default Table;
