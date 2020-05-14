4eb2e657a2e0957d1450d8ab4b7c334c6cfaf7ba
Sky Lin <sky.lin@hulu.com>
2020-05-13 22:25:14 -0700

    Updates

---
+++ precache-manifest.1a7b481381c3aa22cec09ce5e61e8fa0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b40a510548a6333cb43112d99e4b157",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "ddfbd703f9857069b21f",
+    "url": "/react-tutorial/static/css/main.8a18c7c4.chunk.css"
+  },
+  {
+    "revision": "c750ad45df555a5a79a8",
+    "url": "/react-tutorial/static/js/2.e6844fbd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.e6844fbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddfbd703f9857069b21f",
+    "url": "/react-tutorial/static/js/main.e7b31002.chunk.js"
+  },
+  {
+    "revision": "8526c682e0d87d451f0e",
+    "url": "/react-tutorial/static/js/runtime-main.5e5f67a8.js"
+  }
+]);
